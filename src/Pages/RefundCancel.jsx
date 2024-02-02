e674d8c868327536b0954f1ef0e82082ce87b4fd
Harsh Agrawal <harshiscoding@gmail.com>
2024-02-02 17:27:09 +0530

    abc

--- src/Pages/RefundCancel.jsx
+++ src/Pages/RefundCancel.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const RefundCancel = () => {
   return (
-    <div>RefundCancel</div>
+   <>
+   
+   <div className='text-4xl'>Refund And Cancellation Policy</div>
+
+   <div>As we are taking donations so there will not be any refund of the donation amount.</div>
+   </>
   )
 }
 
